978027be9472b4167fe4f008a45aef28fe9869b2
Owen <owen.bayliss@sky.com>
2022-05-29 21:09:55 +0100

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

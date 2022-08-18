10972d3a24b46090b5323a524294a05a9a2ddd77
Owen <owen.bayliss@sky.com>
2022-08-18 22:53:56 +0100

    Updated.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        // random number generator to be called in RandomSuit and RandomValue methods
+        static Random random = new Random();
+
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            // get a random number from 1 to 4
+            int value = random.Next(1, 5);
+            // return Suit based on number returned
+            if (value == 1) return "Spades";
+            if (value == 2) return "Hearts";
+            if (value == 3) return "Clubs";
+            // if nothing has been returned, return the string Diamonds
+            return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            if (value == 11) return "Jack";
+            if (value == 12) return "Queen";
+            if (value == 13) return "King";
+            return value.ToString();
+        }
+    }
+}
--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -6,7 +6,19 @@ namespace PickRandomCards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of cards to pick:");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number entered was not valid.");
+            }
         }
     }
 }
